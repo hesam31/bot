198f8a15ada7aba59e76b3f857e905ea845975f1
hesam31 <132648195+hesam31@users.noreply.github.com>
2026-06-03 17:52:58 +0330

    Add files via upload

--- main.py
+++ main.py
@@ -11,8 +11,8 @@ from telegram.ext import (
 # =========================
 # CONFIG
 # =========================
-BOT_TOKEN = os.getenv("BOT_TOKEN")
-ADMIN_ID = int(os.getenv("ADMIN_ID", "0"))
+BOT_TOKEN = ("8997328313:AAFX3RUfdDAvgLf0SqJ4Z7IPWy00cQCYiWE")
+ADMIN_IDS = [81469723]
 DATABASE_URL = os.getenv("DATABASE_URL")
 
 # =========================
